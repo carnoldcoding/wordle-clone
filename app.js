bea06f2b9caa297d405cd0ca17f1a795162be3dd
carnoldcoding <carnoldcoding@gmail.com>
2022-03-10 14:17:42 -0500

    basic functionality finished

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
 const container = document.querySelector(".container");
-const letters = document.querySelectorAll(".letter");
+const letters = document.querySelectorAll(".letter-box");
 let space = 0;
+let letterCount = 0;
 
 window.addEventListener("keydown", (e) => {
+    
+    //Handle adding letters.
     if((e.key >= "a" && e.key <="z") || e.key == "Backspace"){
-        if(e.key != "Backspace")
+        if(e.key != "Backspace" && letterCount < 5){
             letters[space].textContent = `${e.key}`.toUpperCase();
-
-        if(space < 4 && e.key != "Backspace"){
+            letters[space].classList.toggle("active");
             space += 1;
+            letterCount += 1;
         }
-        
-        if(e.key == "Backspace"){
-            letters[space].textContent = ``.toUpperCase();
-            if(space>0){
-                space -= 1;
-            }
+
+        if(e.key == "Backspace" && letterCount > 0){
+            space-=1;
+            letterCount-=1;
+            letters[space].textContent = '';
+            letters[space].classList.toggle("active");
         }
     }
         
